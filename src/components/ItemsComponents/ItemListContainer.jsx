cfbc3a71cacc4a1874059a2b54696b3eba78bb6f
marianorigo18 <marianowagnerrigo@gmail.com>
2023-03-25 11:29:50 -0300

    fix commit

--- src/components/ItemsComponents/ItemListContainer.jsx
+++ src/components/ItemsComponents/ItemListContainer.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const ItemListContainer = () => {
   return (
-    <div>ItemListContainer</div>
+    <div className='item-list-container'>ItemListContainer</div>
   )
 }
--- src/components/NavbarComponents/BubbleCategories.jsx
+++ src/components/NavbarComponents/BubbleCategories.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 
 export const BubbleCategories = () => {
   return (
-    <div>
+    <>
     <ul className="hidden w-44 bg-slate-600 font-bold justify-between text-yellow-200 sm:flex">
         <li><Link exact to={"/industria"}>industria</Link></li>
         <li><Link exact to={"/iluminacion"}>iluminacion</Link></li>
     </ul>
-    </div>
+    </>
   )
 }
