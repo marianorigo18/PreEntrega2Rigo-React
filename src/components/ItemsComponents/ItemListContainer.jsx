46e1cd0df9df25ca3056042d42040115217a1c90
marianorigo18 <marianowagnerrigo@gmail.com>
2023-03-27 16:01:10 -0300

    fix commit

--- src/components/ItemsComponents/ItemListContainer.jsx
+++ src/components/ItemsComponents/ItemListContainer.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const ItemListContainer = ({children}) => {
   return (
-    <div className='item-list-container'>{children}</div>
+    <div className='item-list-container grid grid-cols-6 gap-3'>{children}</div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { ItemListContainer } from '../components'
-import data from "../data/data.json"
+import dates from "../data/data.json"
 
 export const Home = () => {
-  console.log(data)
   return (
     <div>
       <ItemListContainer>
-        <h1>categorias en ruta /</h1>
+      {dates.categorias.map((product) => (
+            <div className="product__card">
+              <img className="product__img" src={product.img} alt={product.title}/>
+                <p className="product__title">{product.title}</p>
+            </div>
+          ))}
       </ItemListContainer>
     </div>
   )
